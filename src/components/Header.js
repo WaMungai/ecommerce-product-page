f694dc69c1f8ff309b558f1cd2cc87f14c85247e
The Passionate Programmer <wamungai10@gmail.com>
2024-01-08 11:10:05 +0300

    Install and configure tailwind

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React,{useState} from 'react'
+import logo from "../../images/logo.svg"
+import {AiOutlineShoppingCart} from "react-icons/ai"
+import avatar from "../../images/image-avatar.png"
+import menu from "../../images/icon-menu.svg"
+import close from "../../images/icon-close.svg"
+import Cart from './Cart'
+
+
+function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [cartIsOpen,setCartIsOpen] = useState(false);
+
+
+  return (
+   <header>
+    
+   </header>
+  )
+}
+
+export default Header
